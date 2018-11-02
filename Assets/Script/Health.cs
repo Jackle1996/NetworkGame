e93fc74c04a861690df12404559dc05ecaa92c8a
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2018-11-02 18:22:51 +0100

    add everything

---
+++ Assets/Script/Health.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Health : NetworkBehaviour
+{
+    private const int maxHealth = 100;
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+    [SerializeField]
+    private RectTransform healthBar;
+    private NetworkStartPosition[] spawnPoints;
+
+    /// <summary>
+    /// Get all available spawn positions
+    /// </summary>
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (isServer)
+        {        
+            currentHealth -= amount;
+            if (currentHealth <= 0)
+            {
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+        }
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    /// <summary>
+    /// Invoked by server but executed on all clients
+    /// </summary>
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, pick one at random
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // Set the player’s position to the chosen spawn point
+            transform.position = spawnPoint;
+        }
+    }
+}
